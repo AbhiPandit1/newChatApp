bbfd4128b305114be6de2420fbec96c81bcdc83d
Abhishek <224abhishekjha@gmail.com>
2024-07-20 01:55:03 +0100

    Intergerated mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,30 +2,45 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
 
 import authRoutes from './routes/authRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import { app, server } from './socket/socket.js';
-import connectToMongoDb from './db/connectToMongoDB.js';
 
+// Load environment variables from .env file
 dotenv.config();
 
 const __dirname = path.resolve();
-// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+// Middleware setup
+app.use(express.json()); // To parse JSON payloads
 app.use(cookieParser());
 
+// Route setup
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
+// Serve static files
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-server.listen(PORT, () => {
-  connectToMongoDb()
-  console.log(connectToMongoDb)
-  console.log(`Server Running on port ${PORT}`);
-});
+// Connect to MongoDB and start the server
+const startServer = async () => {
+  try {
+    // Connect to MongoDB
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log(`MongoDB is Connected ${process.env.MONGO_DB_URI}`);
+
+    // Start the server
+    server.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error('Error starting server:', err.message);
+  }
+};
+
+startServer();
